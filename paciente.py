d4209b83208e06292b06633f25e99240e427652f
ZuinMuis <jcolomaeduardo@gmail.com>
2026-09-21 15:37:21 +0000

    Modificacion texto

--- paciente.py
+++ paciente.py
@@ -1,23 +1,49 @@
-class Paciente 
-    def __init__(self, rut:str, nombre:str, edad:int, prevision:str):
+class Paciente:
+    
+PREVISIONES: set[str] = {"Fonasa", "Isapre", "Particular"}
+
+
+
+    def __init__(self, rut: str, nombre: str, edad: int, prevision: str) -> None:
         self.rut = rut
         self.nombre = nombre
         self.edad = edad
         self.prevision = prevision
 
-        @roperty
-        def rut(self)->str:
-            return self._rut
-        @rut.setter
-        def rut(self, rut:str)->None:
-            self._rut = rut
+    @property
+    def rut(self) -> str:
+        return self._rut
 
-        @property
-        def nombre(self)->str:
-            return self._nombre
+    @rut.setter
+    def rut(self, rut: str) -> None:
+        self._rut = rut
 
-        @nombre.setter
-        def nombre(self, nombre:str)->None:
-            self._nombre = nombre
-            
+    @property
+    def nombre(self) -> str:
+        return self._nombre
 
+    @nombre.setter
+    def nombre(self, nombre: str) -> None:
+        self._nombre = nombre
+
+    @property
+    def edad(self) -> int:
+        return self._edad
+
+    @edad.setter
+    def edad(self, edad: int) -> None:
+        self._edad = edad
+
+    @property
+    def prevision(self) -> str:
+        return self._prevision
+
+    @prevision.setter
+    def prevision(self, prevision: str) -> None:
+        self._prevision = prevision
+
+    def __str__(self) -> str:
+        return f"Informacion del paciente:\nRUT: {self.rut}\nNombre: {self.nombre}\nEdad: {self.edad}\nPrevision: {self.prevision}"
+
+    def __repr__(self) -> str:
+        return f"Paciente(rut='{self.rut}', nombre='{self.nombre}', edad={self.edad}, prevision='{self.prevision}')"
